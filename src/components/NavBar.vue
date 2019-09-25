57e543be3c3a16025f7532eebe58435042d318ff
djbios <dorandval@gmail.com>
2019-09-25 18:38:32 +0300

    WIP

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -5,34 +5,35 @@
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="collapsibleNavbar">
-<!--            <ul class="navbar-nav">-->
-<!--                {% for t in types %}-->
-<!--                <li class="nav-item">-->
-<!--                    <a class="nav-link" href="{% url 'by_type' t %}">{{ t }}</a>-->
-<!--                </li>-->
-<!--                {% endfor %}-->
-<!--            </ul>-->
-
             <ul class="navbar-nav">
                 <li :key="tag.name" v-for="tag in tags">
-
-                    <a class="nav-link" href="{% url 'by_type' t %}">{{ tag.name }}</a>-->
+                    <a class="nav-link" href="{% url 'by_type' t %}">{{ tag.name }}</a>
                 </li>
             </ul>
         </div>
+        <p>{{js}}</p>
     </nav>
 </template>
 
 <script>
+    import axios from 'axios';
+
     export default {
         name: "NavBar",
+        vars: {},
         data () {
             return {
                 tags:[
                     {name: 'sdfdsf'},
                     {name: 'sdg'}
-                ]
+                ],
+                js:""
             }
+        },
+        mounted() {
+            axios
+                .get('https://api.coindesk.com/v1/bpi/currentprice.json')
+                .then(response => (this.js = response.data.bpi.USD));
         }
     }
 
